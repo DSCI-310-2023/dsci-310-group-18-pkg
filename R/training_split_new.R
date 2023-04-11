b1af0b96afd12a797af3208b3cf752f5f1463b8c
leuneri <ericleung147258369@hotmail.com>
2023-04-10 17:25:50 -0800

    defined training_split_new

---
+++ R/training_split_new.R
@@ -0,0 +1,12 @@
+#' training_split_new
+#'
+#' A dataset containing information the heart
+#'
+#' @format A data frame with 403 rows and 3 variables:
+#'
+#' @name training_split_new
+#'
+#' @export
+#'
+#' @keywords training_split_new
+training_split_new <- readr::read_csv(here::here("data/training_split_new.csv"))
