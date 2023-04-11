ce6db0f080a8f2efe5fda82f5eea68e147f68507
samnthorne <sam.n.thorne@gmail.com>
2023-04-10 22:30:49 -0700

    Added xAxis to boxplot function to fix errors

--- R/grid_boxplot.R
+++ R/grid_boxplot.R
@@ -1,7 +1,8 @@
 #' Make a grid boxplot
 #'
 #' @param data The dataset we're using.
-#' @param yAxis The Y Axis parameter.
+#' @param xAxis The X axis parameter, must be factor type
+#' @param yAxis The Y Axis parameter, must be numeric type
 #' @param yLabel The label for the Y Axis.
 #' @param titleLabel The label for the title.
 #'
@@ -9,10 +10,10 @@
 #' @export
 #'
 #' @examples
-#' grid_boxplot(mtcars, age, "Age (years)",
+#' grid_boxplot(mtcars, diagnosis_f, age, "Age (years)",
 #' "A. Boxplot of degree of heart \n disease in relation to patient's \nage")
-grid_boxplot <- function(data, yAxis, yLabel, titleLabel){
-  boxplot_age <- ggplot2::ggplot(data, ggplot2::aes(x="diagnosis_f", y= {{yAxis}})) +
+grid_boxplot <- function(data, xAxis, yAxis, yLabel, titleLabel){
+  boxplot_age <- ggplot2::ggplot(data, ggplot2::aes(x = xAxis, y= {{yAxis}})) +
     ggplot2::geom_boxplot(color="#033291", fill="#b0c8f7", alpha=0.2) +
     ggplot2::labs(x="Degree of heart disease", y=yLabel) +
     ggplot2::ggtitle(titleLabel) +
